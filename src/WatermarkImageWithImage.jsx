72559ac35295e62e6b91d274009630aa731efb9b
enigmaticmahesh <challengermahesh@gmail.com>
2024-05-16 21:37:25 +0530

    few improvements

--- src/WatermarkImageWithImage.jsx
+++ src/WatermarkImageWithImage.jsx
@@ -1,52 +1,66 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import dracula from './assets/badam-pak-2.jpg';
 
 const ALLOWED_FILE_TYPES = ['image/png', 'image/jpeg'];
+
+const watermarkImageWithImage = async (dataUrl) => {
+  const imgElement = document.createElement('img');
+  imgElement.src = dataUrl;
+  const imageWidth = imgElement.width;
+  const imageHeight = imgElement.height;
+  const canvas = document.createElement('canvas');
+  // const canvas = new OffscreenCanvas(imageWidth, imageHeight);
+  const context = canvas.getContext('2d');
+  canvas.width = imageWidth;
+  canvas.height = imageHeight;
+
+  context.drawImage(imgElement, 0, 0, imageWidth, imageHeight);
+
+  // console.log(dracula);
+  const res = await fetch(dracula);
+  // console.log({ res });
+  const blob = await res.blob();
+  // console.log({ blob });
+  const image = await createImageBitmap(blob);
+  // console.log({ image });
+  const pattern = context.createPattern(image, 'no-repeat');
+
+  context.translate(imageWidth - image.width, imageHeight - image.height);
+  context.rect(0, 0, imageWidth, imageHeight);
+  context.fillStyle = pattern;
+
+  context.fill();
+  canvas.remove();
+  imgElement.remove();
+
+  return canvas.toDataURL();
+};
+
+const WatermarkedImage = ({ dataUrl }) => {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    const attachWatermark = async (imgDataUrl) => {
+      const wImage = await watermarkImageWithImage(imgDataUrl);
+      setImage(wImage);
+    };
+    if (dataUrl) {
+      attachWatermark(dataUrl);
+    }
+  }, [dataUrl]);
+
+  return image ? <img src={image} alt="Watermarked_User_Img" /> : null;
+};
 
 const WatermarkImageWithImage = () => {
   const inputFileRef = useRef();
   const userImgRef = useRef();
   const [file, setFile] = useState(null);
-  const [watermarkedFile, setWatermarkedFile] = useState(null);
-
-  const watermarkImageWithImage = async (imgElement) => {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    const imageWidth = imgElement.width;
-    const imageHeight = imgElement.height;
-    canvas.width = imageWidth;
-    canvas.height = imageHeight;
-
-    context.drawImage(imgElement, 0, 0, imageWidth, imageHeight);
-
-    // console.log(dracula);
-    const res = await fetch(dracula);
-    // console.log({ res });
-    const blob = await res.blob();
-    // console.log({ blob });
-    const image = await createImageBitmap(blob);
-    // console.log({ image });
-    const pattern = context.createPattern(image, 'no-repeat');
-
-    context.translate(imageWidth - image.width, imageHeight - image.height);
-    context.rect(0, 0, imageWidth, imageHeight);
-    context.fillStyle = pattern;
-
-    context.fill();
-    setWatermarkedFile(canvas.toDataURL());
-    canvas.remove();
-  };
 
   const readUserFile = (userFile) => {
     const reader = new FileReader();
     reader.onload = () => {
       setFile(reader.result);
-      setTimeout(() => {
-        const attachWatermark = () => {
-          watermarkImageWithImage(userImgRef.current);
-        };
-        attachWatermark();
-      }, 1000);
     };
     reader.readAsDataURL(userFile);
   };
@@ -70,9 +84,7 @@ const WatermarkImageWithImage = () => {
   };
 
   const userImage = file && <img src={file} alt="User_Img" />;
-  const watermarkedImage = watermarkedFile && (
-    <img src={watermarkedFile} alt="Watermarked_User_Img" />
-  );
+  const watermarkedImage = file && <WatermarkedImage dataUrl={file} />;
 
   return (
     <div>
